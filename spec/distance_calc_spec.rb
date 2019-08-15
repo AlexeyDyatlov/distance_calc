eaa03e21074440c7ad3e206eccf37ce28be7f5b3
alex <tgh88@mail.ru>
2019-08-15 16:03:02 +0300

    change version

--- spec/distance_calc_spec.rb
+++ spec/distance_calc_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe DistanceCalc do
   end
 
   describe '.calc' do
-    let!(:response) { DistanceCalc.calc(point1, point2) }
     let!(:point1) { { lat: 41.8892699, long: 12.4923319 } }
     let!(:point2) { { lat: 41.8886001, long: 12.4903215 } }
+    let!(:response) { DistanceCalc.calc(point1, point2) }
 
     it 'returns distance' do
       expect(response).to be_kind_of(Integer)
